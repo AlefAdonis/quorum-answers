2bf7aa0030752976f253a6d1fdc4b73e5e47595b
AlefAdonis <alefadonis10@gmail.com>
2023-11-16 10:36:39 -0300

    [add] Adding the answer to the first question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import datetime
 import os
+import numpy as np
+import pandas as pd
 
 # File info for Input data
 DATA_DIR = "./data/"
@@ -41,4 +43,33 @@ if __name__ == "__main__":
         log.error(f"There is input data missing! Please check the data dir {DATA_DIR} and rerun the script.")
 
     log.info("Extracting the data from csv files.")
+    bill_df = pd.read_csv(DATA_DIR+BILL_FILE, delimiter=",")
+    legis_df = pd.read_csv(DATA_DIR+LEGIS_FILE, delimiter=",")
+    results_vote_df = pd.read_csv(DATA_DIR+RESULTS_FILE, delimiter=",")
+    votes_df = pd.read_csv(DATA_DIR+VOTE_FILE, delimiter=",")
 
+    log.info("Answering the First Question")
+
+    # renaming the id from the legis_df to match in the merge
+    legis_df.rename({"id": "legislator_id"}, axis=1, inplace=True)
+    bills_count = results_vote_df.merge(legis_df, on="legislator_id", how="left")
+
+    # creating a copy of the legislator data to use the data without Data View problems
+    result_of_legis_df = legis_df.copy()
+    result_of_legis_df[["num_supported_bills", "num_opposed_bills"]] = 0
+    for b in bills_count.itertuples():
+        if b.vote_type == 1:
+            if not result_of_legis_df.loc[result_of_legis_df["legislator_id"] == b.legislator_id, "num_supported_bills"].isnull().any():
+                result_of_legis_df.loc[result_of_legis_df["legislator_id"] == b.legislator_id, "num_supported_bills"] += 1
+            else:
+                result_of_legis_df.loc[result_of_legis_df["legislator_id"] == b.legislator_id, "num_supported_bills"] = 1
+            continue
+
+        if b.vote_type == 2:
+            if not result_of_legis_df.loc[result_of_legis_df["legislator_id"] == b.legislator_id, "num_opposed_bills"].isnull().any():
+                result_of_legis_df.loc[result_of_legis_df["legislator_id"] == b.legislator_id, "num_opposed_bills"] += 1
+            else:
+                result_of_legis_df.loc[result_of_legis_df["legislator_id"] == b.legislator_id, "num_opposed_bills"] = 1
+
+    result_of_legis_df.to_csv(DATA_DIR+COUNT_VOTES_LEGIS_FILE, sep=",", index=False)
+    pass
